a66cd8b97cd4834e4264c2118adb768ea05ed4bb
TOHAD <@ap97824@gmail.com>
2025-11-09 02:09:16 +0300

    added diagrammPage code

--- Pages/DiagrammPage.xaml.cs
+++ Pages/DiagrammPage.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Windows.Forms.DataVisualization.Charting;
 using Word = Microsoft.Office.Interop.Word;
 using Excel = Microsoft.Office.Interop.Excel;
+using _222_Goman_WPF_Project.DBModel;
 
 namespace _222_Goman_WPF_Project.Pages
 {
@@ -23,14 +24,35 @@ namespace _222_Goman_WPF_Project.Pages
     /// </summary>
     public partial class DiagrammPage : Page
     {
+        private Goman_DB_Payment0Entities _context = new Goman_DB_Payment0Entities();
         public DiagrammPage()
         {
             InitializeComponent();
+            ChartPayments.ChartAreas.Add(new ChartArea("Main"));
+            var currentSeries = new Series("Платежи")
+            {
+                IsValueShownAsLabel = true
+            };
+            ChartPayments.Series.Add(currentSeries);
+            CmbBxUsers.ItemsSource = _context.Users.ToList(); //ФИО пользователей
+            CmbBxDiagramm.ItemsSource = Enum.GetValues(typeof(SeriesChartType)); //Типы диаграммы
         }
 
         private void UpdateChart(object sender, SelectionChangedEventArgs e)
         {
+            if (CmbBxUsers.SelectedItem is Users currentUser && CmbBxDiagramm.SelectedItem is SeriesChartType currentType)
+            {
+                Series currentSeries = ChartPayments.Series.FirstOrDefault();
 
+                currentSeries.ChartType = currentType;
+                currentSeries.Points.Clear();
+
+                var categoriesList = _context.Categories.ToList();
+                foreach (var category in categoriesList)
+                {
+                    currentSeries.Points.AddXY(category.Name, _context.Payments.ToList().Where(u => u.Users == currentUser && u.Categories == category).Sum(u => u.Price * u.Num));
+                }
+            }
         }
     }
 }
